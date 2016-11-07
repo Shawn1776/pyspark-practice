303ae551e563a75b40d78a77af99a109b2b39e1a
Shawn Zhang <Shawn1776@users.noreply.github.com>
2016-11-07 16:58:50 -0500

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,1 +1,4 @@
-# from 27 mins--> flatmap, a list of list 
+# from 27 mins--> flatmap, a list of list, pythn has no flatmap but chain can work round of it.
+from intertools import chain
+# pyspark starts at 36mins (set speed to 1.25x, else too slow)
+
